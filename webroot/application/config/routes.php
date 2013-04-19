1d3187f2ca941fcf183b1adc0d598d044e03025c
ali mert celik <celik.alimert@gmail.com>
2013-04-19 11:46:41 +0300

    test email smtp v.02

--- webroot/application/config/routes.php
+++ webroot/application/config/routes.php
@@ -42,7 +42,6 @@ $route['default_controller'] = 'home';
 $route['entry/:num'] = 'entry/index/$1/$1';
 $route['home/:num'] = 'home/index/$1';
 $route['rss/:num'] = 'rss/entries/$1';
-
 //$route['entry/:num'] = 'entry/edit_entry/$1';
 
 /* End of file routes.php */
